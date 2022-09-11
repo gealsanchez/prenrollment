75ad3ff8e6c8d4c89a9dde040410a09d84da47d2
gealsanchez <gealsanchez@gmail.com>
2022-09-11 17:09:53 -0500

    Video Player & Suggestions

--- youtube.js
+++ youtube.js
@@ -2,9 +2,11 @@ const divnav = document.createElement('div');
 divnav.id = "navigation"
 container.appendChild(divnav);
 
+//******************* */ icons
+
 const img = document.createElement('img')
-img.className="lefticon"
-img.src="tubeicon.png"
+img.className = "lefticon"
+img.src = "tubeicon.png"
 divnav.appendChild(img)
 
 const input = document.createElement('input')
@@ -12,27 +14,68 @@ input.setAttribute = ("type", "text")
 divnav.appendChild(input)
 
 const imgI = document.createElement('img')
-imgI.src="glassicon.png"
+imgI.src = "glassicon.png"
 divnav.appendChild(imgI)
 
 const imgII = document.createElement('img')
-imgII.src="micon.png"
+imgII.src = "micon.png"
 divnav.appendChild(imgII)
 
 const imgIII = document.createElement('img')
-imgIII.className="righticon"
-imgIII.src="camicon.png"
+imgIII.className = "righticon"
+imgIII.src = "camicon.png"
 divnav.appendChild(imgIII)
 
+//****************** */ container div, videoplayer % suggestions
+
 const divcon = document.createElement('div');
-divcon.id = "containerI"
-divcon.textContent = "containerI"
+divcon.id = "containerI";
 container.appendChild(divcon);
 
+//***************** */ youtube player div
+
 const divplay = document.createElement('div');
-divplay.textContent = "player div"
+divplay.id = "divplayer";
 divcon.appendChild(divplay);
 
+const vidplar = document.createElement('iframe')
+vidplar.src = "https://www.youtube.com/embed/EceJQ05KTf4"
+vidplar.width = "890"
+vidplar.height = "500"
+vidplar.title = "Youtube Video Player"
+vidplar.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+vidplar.allowFullscreen = true;
+divplay.appendChild(vidplar)
+
+//***************** */ suggestions div
+
 const divsug = document.createElement('div');
-divsug.textContent = "suggestion div"
+divsug.id = "suggestion"
 divcon.appendChild(divsug);
+
+const vidsug = document.createElement('iframe')
+vidsug.src = "https://www.youtube.com/embed/1PnVor36_40"
+vidsug.width = "240"
+vidsug.height = "180"
+vidsug.title = "Youtube Video Player"
+vidsug.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+vidsug.allowFullscreen = true;
+divsug.appendChild(vidsug)
+
+const vidsugI = document.createElement('iframe')
+vidsugI.src = "https://www.youtube.com/embed/pQN-pnXPaVg"
+vidsugI.width = "240"
+vidsugI.height = "180"
+vidsugI.title = "Youtube Video Player"
+vidsugI.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+vidsugI.allowFullscreen = true;
+divsug.appendChild(vidsugI)
+
+const vidsugII = document.createElement('iframe')
+vidsugII.src = "https://www.youtube.com/embed/PkZNo7MFNFg"
+vidsugII.width = "240"
+vidsugII.height = "180"
+vidsugII.title = "Youtube Video Player"
+vidsugII.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+vidsugII.allowFullscreen = true;
+divsug.appendChild(vidsugII)
